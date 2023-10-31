44c25b7a28d4b7e33fc39dffb32ede5100df28a4
hellcat-M18 <136050032+hellcat-M18@users.noreply.github.com>
2023-10-31 11:21:49 +0900

    Modified index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serverlessExpress = require('@vendia/serverless-express')
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("Wheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!!!")
 })
 
 if (process.env.NODE_ENV === `develop`) app.listen(port)
